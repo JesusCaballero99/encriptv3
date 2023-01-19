54c8f79025044333375fe01d44e8ea2e5d50c667
JesusCaballero99 <caballerojesusgabriel@gmail.com>
2023-01-18 17:21:01 -0700

    capturando datos del textbox

--- logica.js
+++ logica.js
@@ -1,1 +1,19 @@
-const inputTexto = document.getElementsByClassName("text-area")
+const btnEncriptar = document.querySelector("#btn-encriptar");
+const inputTexto = document.querySelector("#text-area");
+
+function encriptarTexto(e){
+    e.preventDefault();
+    let texto = inputTexto.value;
+    alert(texto);
+    let longitud = (inputTexto.value).length;
+    alert(longitud);
+    
+    for(let i=0;i<=longitud-1;i++){
+        alert(texto[i]);
+    }
+
+    
+
+}
+
+btnEncriptar.onclick = encriptarTexto;
