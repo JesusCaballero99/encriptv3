aabd6e9bcd941eac23b9d7fc3020f87debec2ecf
JesusCaballero99 <caballerojesusgabriel@gmail.com>
2023-01-18 18:53:02 -0700

    terminando el script de la logica del encriptador

--- logica.js
+++ logica.js
@@ -1,19 +1,40 @@
 const btnEncriptar = document.querySelector("#btn-encriptar");
 const inputTexto = document.querySelector("#text-area");
+const btnDesencriptar = document.querySelector("#btn-desencriptar");
+const btnCopiar = document.querySelector("#btn-copiar");
+const mensaje = document.querySelector("#mensaje");
+
 
 function encriptarTexto(e){
     e.preventDefault();
     let texto = inputTexto.value;
-    alert(texto);
-    let longitud = (inputTexto.value).length;
-    alert(longitud);
-    
-    for(let i=0;i<=longitud-1;i++){
-        alert(texto[i]);
-    }
+    let textocifrado=texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
 
+    mensaje.value = textocifrado;
     
+    mensaje.focus();
 
 }
 
+function desencriptarTexto(e){
+    e.preventDefault();
+    let texto = inputTexto.value;
+    let textodescifrado = texto.replace(/enter/gi,"e").replace(/imes/gi,"i").replace(/ai/gi,"a").replace(/ober/gi,"o").replace(/ufat/gi, "u");
+    mensaje.value = textodescifrado;
+
+    mensaje.focus();
+    
+}
+
+function copiarTexto(){
+  
+    mensaje.select();
+    document.execCommand("copy");
+    
+
+    alert("texto copiado en el portapapeles");
+}
+
 btnEncriptar.onclick = encriptarTexto;
+btnDesencriptar.onclick = desencriptarTexto;
+btnCopiar.onclick = copiarTexto;
